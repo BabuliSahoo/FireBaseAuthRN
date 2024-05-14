a6bc047f914c15bfa062ca3ad1efdf96470ce75f
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-05-14 18:38:22 +0530

    Sign Up and Login functionality Added

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyDCYasArcOwcALFhIj2szug5aD2PgUQu1E'
+const API_KEY = 'AIzaSyBU1yaeYhSbBco2kAZsEpuqdO-yPudJYTs';
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
-  );
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+
+  return token;
+}
+
+export function createUser(email, password) {
+  return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+  return authenticate('signInWithPassword', email, password);
 }
